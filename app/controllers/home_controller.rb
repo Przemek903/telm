331cc552e46ca2d939858f3ce1ff97388ca796b9
Przemyslaw Kuc <przemek903@gazeta.pl>
2014-02-01 14:54:51 +0100

    final ho asd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,6 @@
 class HomeController < ApplicationController
   def help
-    @patients = Patient.all
-    @medicines = Medicine.all
-    
-    respond_to do |format|
-    format.html 
-    format.json { render json: @medicines }
-    
-  end
+
 end
   
   def home 
